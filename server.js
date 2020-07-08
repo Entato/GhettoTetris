a1450b23c00b7b96b01898fce991099ff74ba40c
Kai Xu <48327213+Entato@users.noreply.github.com>
2020-07-08 13:41:50 -0400

    changed port to 3000

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const game = require("./game.js");
 
 //creating web server
 const app = express();
-const server = app.listen(80, function () {
-    console.log("listening to request on port 80");
+const server = app.listen(3000, function () {
+    console.log("listening to request on port 3000");
 });
 app.use(express.static("public"));
 
